68dbfb1651ba2e2fde8ae181debdb2d90056aa9e
Viktor Luft <viktor.luft@freiheit.com>
2025-06-03 16:48:21 +0200

    Format files (#3)

--- index.ts
+++ index.ts
@@ -1,23 +1,23 @@
 type WithErrResultPlain<T> = [Error, undefined] | [undefined, T];
 type WithErrResult<T> = T extends Promise<infer U>
-    ? Promise<WithErrResultPlain<U>>
-    : WithErrResultPlain<T>;
+  ? Promise<WithErrResultPlain<U>>
+  : WithErrResultPlain<T>;
 type WithErr<T> = T extends Promise<unknown> ? WithErrResult<T>
-    : T extends () => infer U ? WithErrResult<U>
-    : never;
+  : T extends () => infer U ? WithErrResult<U>
+  : never;
 
 const wrapErr = (err: unknown) =>
-    err instanceof Error
-        ? err
-        : new Error(`Non-error object was thrown or rejected: ${err}`);
+  err instanceof Error
+    ? err
+    : new Error(`Non-error object was thrown or rejected: ${err}`);
 
 const handleProm = <T extends Promise<unknown>>(
-    p: Promise<T>,
+  p: Promise<T>,
 ): Promise<WithErrResultPlain<T>> =>
-    p.then<WithErrResultPlain<T>, WithErrResultPlain<T>>(
-        (r) => [undefined, r],
-        (err) => [wrapErr(err), undefined],
-    );
+  p.then<WithErrResultPlain<T>, WithErrResultPlain<T>>(
+    (r) => [undefined, r],
+    (err) => [wrapErr(err), undefined],
+  );
 
 /**
  * This function is designed to return an error as first param of the returned tuple, that would require
@@ -27,17 +27,17 @@ const handleProm = <T extends Promise<unknown>>(
  * @returns The potential error and response as a tuple.
  */
 export const withErr = <T extends (() => unknown) | Promise<unknown>>(
-    funcOrProm: T,
+  funcOrProm: T,
 ): WithErr<T> => {
-    if (funcOrProm instanceof Promise) {
-        return handleProm(funcOrProm) as WithErr<T>;
-    }
+  if (funcOrProm instanceof Promise) {
+    return handleProm(funcOrProm) as WithErr<T>;
+  }
 
-    try {
-        const r = funcOrProm();
-        if (r instanceof Promise) return handleProm(r) as WithErr<T>;
-        return [undefined, r] as WithErr<T>;
-    } catch (err) {
-        return [wrapErr(err), undefined] as WithErr<T>;
-    }
+  try {
+    const r = funcOrProm();
+    if (r instanceof Promise) return handleProm(r) as WithErr<T>;
+    return [undefined, r] as WithErr<T>;
+  } catch (err) {
+    return [wrapErr(err), undefined] as WithErr<T>;
+  }
 };
